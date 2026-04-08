4da02dfa5a982159b367b77ed1daedff5bfa706d
mira3eee <gamelol987654@gmail.com>
2026-04-09 01:01:31 +1000

    опять переделала .gitignore + добавила кода в проект

--- work/progect/progect/progect.cpp
+++ work/progect/progect/progect.cpp
@@ -1,7 +1,18 @@
 ﻿#include <iostream>
-#include <windows.h>
+#include <windows.h> // Sleep, Beep, GetlLcalTime
 using namespace std;
 
 int alarmhour;
 int alarmminute;
+bool alarmEnabled = false;
+
+void setAlarm()
+{
+	cout << "vvedite chas budilnica: ";
+	cin >> alarmhour;
+	cout << "vvedite minutes budilnica: ";
+	cin >> alarmminute;
+
+	alarmEnabled = true;
+}
 
