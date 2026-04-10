f6ffe5cc959cd6d619d89cee346e3fd4a643a455
mira3eee <gamelol987654@gmail.com>
2026-04-10 12:21:52 +1000

    код работает

--- work/progect/progect/progect.cpp
+++ work/progect/progect/progect.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int alarmhour;
 int alarmminute;
+int alarmsecond;
 bool alarmEnabled = false;
 
 void setAlarm()
@@ -12,25 +13,30 @@ void setAlarm()
 	cin >> alarmhour;
 	cout << "vvedite minutes budilnica: ";
 	cin >> alarmminute;
+	cout << "vvedite seconds budilnica: ";
+	cin >> alarmsecond;
+
 
 	alarmEnabled = true;
 	cout << "budilnik ustanovlen na ";
-	cout << alarmhour << ":" << alarmminute;
+	cout << alarmhour << ":" << alarmminute << ":" << alarmsecond << endl;
 }
 
 void ringAlarm()
 {
 	cout << "Budilnik Ring!!!!!!!!!!!" << endl;
-	cout << "Najmite probel dla otkluchenia" << endl;
 		
-	while (alarmEnabled)
+	while (true)
 	{
-		Beep(1000, 500);
+		cout << "ALAAAARM!!!!" << endl;
+		Beep(1000, 1000);
+		cout << "Najmite probel dla otkluchenia" << endl;
 
-		if (GetAsyncKeyState(VK_SPACE))
+		if (GetAsyncKeyState(VK_SPACE) & 0x8000)
 		{
 			alarmEnabled = false;
 			cout << "budilnik vicluchen";
+			break;
 		}
 
 		Sleep(100);
@@ -44,17 +50,24 @@ void CheckAlarm()
 
 	int chour = t.wHour;
 	int cminute = t.wMinute;
+	int csecond = t.wSecond;
 
 	if (alarmEnabled &&
 		chour == alarmhour &&
-		cminute == alarmminute)
+		cminute == alarmminute &&
+		csecond == alarmsecond)
 	{
-		ringAlarm;
+		cout << "\nproverca" << endl;
+		ringAlarm();
 	}
 }
 
 int main()
 {
+	cout << "programma zapuzhena" << endl;
+
+	setAlarm();
+
 	while (true)
 	{
 		CheckAlarm();
